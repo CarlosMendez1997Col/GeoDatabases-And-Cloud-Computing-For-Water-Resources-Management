c24880b370d64c13973d1746f8c16acc5aecd87c
Carlos Mendez <carlos.mendez@unesp.br>
2025-08-24 19:53:43 -0500

    Create CreationGeodatabases.sql

---
+++ CreationGeodatabases.sql
@@ -0,0 +1,14 @@
+--- Water Resources Management using PostgreSQL and PgAdmin4
+--- create by MSc Carlos Mendez
+
+CREATE DATABASE master_gdb
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'Spanish_Spain.1252'
+    LC_CTYPE = 'Spanish_Spain.1252'
+    LOCALE_PROVIDER = 'libc'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
+
