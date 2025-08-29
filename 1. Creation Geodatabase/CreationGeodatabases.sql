3a41f8a57a22f4895f5a2e661980188246410113
Carlos Mendez <carlos.mendez@unesp.br>
2025-08-28 20:50:24 -0500

    update data

--- 1. Creation Geodatabase/CreationGeodatabases.sql
+++ 1. Creation Geodatabase/CreationGeodatabases.sql
@@ -1,7 +1,40 @@
---- Water Resources Management using PostgreSQL and PgAdmin4
---- Developed by MSc Carlos Mendez
+/*
+											Water Resources Managemen using PostgreSQL and PgAdmin4
+															Area of Interest (South America)
+															 Developed by MSc Carlos Mendez
+																														 
+MOST TABLES AND DATASETS USED:
 
---- Create DATABASE
+HydroSHEDS
+https://www.hydrosheds.org/
+
+1. HydroSHEDS
+2. HydroBASINS
+3. HydroRIVERS
+4. HydroLAKES
+5. Global Lakes and Wetlands Database (GLWD)
+6. HydroWASTE
+7. Global River Classification (GloRiC)
+8. Lake TEMP
+
+World Resources Institute
+https://www.wri.org/
+
+9. Global Power Plant Database
+
+Global Administrative Unit Layers (GAUL) 
+
+10. Global Country boundaries
+11. Firt Level Administrative Units
+12. Second Level Administrative Units
+
+*/
+
+--- General Steps
+
+--- Register a new SERVER, for example 'Water_Server'
+
+--- Create a new DATABASE, for example 'master_gdb' 
 CREATE DATABASE master_gdb
     WITH
     OWNER = postgres
@@ -13,10 +46,7 @@ CREATE DATABASE master_gdb
     CONNECTION LIMIT = -1
     IS_TEMPLATE = False;
 
---- Delete DATABASE
-drop DATABASE master_gdb;	
-
---- Create an user with permissions
+--- Create an user with permissions, for example 'support'
 CREATE ROLE support WITH
 	LOGIN
 	SUPERUSER
@@ -29,11 +59,12 @@ CREATE ROLE support WITH
 	PASSWORD 'xxxxxx';
 COMMENT ON ROLE support IS 'support data';
 
---- delete user with permissions 
-drop ROLE SUPPORT;
-
---- Change the administrator and owner of DATABASE
+--- Change the administrator and set 'Support'
 ALTER DATABASE master_gdb OWNER TO support;
+
+
+
+
 
 --- Create a table with fields
 CREATE TABLE water_categories(Hybas_ID int, water_description varchar(30));
@@ -62,6 +93,18 @@ UPDATE water_categories SET water_description = 'Basin' WHERE hybas_id = 123456;
 --- Delete ROWS
 DELETE FROM water_categories WHERE hybas_id = 123456;
 DELETE FROM water_categories WHERE hybas_id = 7890;
+
+create EXTENSION postgis;
+
+CREATE SCHEMA water_data
+    AUTHORIZATION postgres;
+
+
+--- Delete DATABASE
+drop DATABASE master_gdb;	
+
+--- delete user with permissions 
+drop ROLE SUPPORT;
 
 
 
